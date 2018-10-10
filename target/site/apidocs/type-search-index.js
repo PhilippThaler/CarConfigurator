1d62cb25f950ab979ad8cbaebd5bb6099a13f554
Philipp Thaler <thalerphilipp1@gmail.com>
2018-10-10 14:45:10 +0200

    Added JavaDocs

---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.philippthaler.app","l":"App"},{"p":"com.philippthaler.app","l":"Car"},{"p":"com.philippthaler.app","l":"CarBrand"},{"p":"com.philippthaler.app","l":"CarConfigurator"},{"p":"com.philippthaler.app","l":"Database"},{"p":"com.philippthaler.app","l":"Model"},{"p":"com.philippthaler.app","l":"Package"}]
